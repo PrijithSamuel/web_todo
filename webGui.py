6ccab8c07260575580f12f5cc555faf207b04bd3
priji <prijith.samuel@gmail.com>
2024-03-03 10:28:30 -0700

    Web_project complete

--- webGui.py
+++ webGui.py
@@ -1,8 +1,10 @@
 import streamlit as st
 import functions
 
-todos = functions.get_todos()
 
+
+todos = functions.get_todos()
+st.set_page_config(layout='wide')
 
 def add_todo():
     todo = st.session_state['new_todo'] + '\n'
@@ -14,7 +16,10 @@ def add_todo():
 
 st.title('My ToDo List')
 st.subheader('This is My todo App.')
-st.write('This app is to increase your productivity.')
+st.write('This app is to increase your <b>productivity</b>.', unsafe_allow_html=True) # This to make python understand the HTMl tag to be treated as html
+
+st.text_input(label='Enter ToDo:', placeholder='Add a new todo....', on_change=add_todo,
+              key='new_todo')
 
 for index, todo in enumerate(todos):
     checkbox = st.checkbox(todo, key=todo)
@@ -25,7 +30,6 @@ for index, todo in enumerate(todos):
         st.rerun()#st.experimental_rerun()
 
 
-st.text_input(label='Enter ToDo:', placeholder='Add a new todo....', on_change=add_todo,
-              key='new_todo')
+
 
 # st.session_state
